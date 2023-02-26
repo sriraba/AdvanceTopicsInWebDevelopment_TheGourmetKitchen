f3262fb35a3b741f00f7a796f316552a5047bdae
Aneri Shah <an235252@dal.ca>
2023-02-26 14:49:15 -0400

    Navigate via app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Description from './Components/Description';
-import FeedBack from './Components/FeedBack';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Description />
-      <FeedBack />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Description />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
