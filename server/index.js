6bce9de93a8b367e713738789f1e24293bc20ec6
Sri Ramya Basam <sr881461@dal.ca>
2023-03-31 02:35:03 -0300

    resolve CORS

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const express = require("express");
 const menuRoutes = require("./Routes/menuRoutes");
 const dbConnection = require("./DBConnection/db");
 const usersRouter = require('./routes/users.route');
+const cors = require('cors');
 
 dbConnection();
 
 const app = express();
 
 app.use(express.json());
+const corsOptions = {
+  origin: '*',
+  methods: 'GET, POST, PATCH, PUT, DELETE',
+  optionsSuccessStatus: 204
+}
+app.use(cors(corsOptions));
+
 
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
